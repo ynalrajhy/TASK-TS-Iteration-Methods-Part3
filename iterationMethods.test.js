2604eb4a9713b617b4324b73d08c4a68a4ad16cb
Octowl <alsaff1987@gmail.com>
2024-11-04 01:26:00 +0300

    better test org

--- iterationMethods.test.js
+++ iterationMethods.test.js
@@ -1,5 +1,4 @@
 const {
-  temperatures,
   filterHighTemperatures,
   filterLowTemperatures,
   convertCelsiusToFahrenheit,
@@ -8,36 +7,57 @@ const {
   getMinTemperature,
 } = require("./iterationMethods.js");
 
+const temperatures = [
+  22, 25, 19, 24, 28, 30, 21, 20, 27, 29, 23, 26, 24, 22, 18, 19, 21, 28, 30,
+  27, 26, 25, 22, 23, 19, 20, 28, 29, 27, 26,
+];
+
 describe("Temperature Analysis and Transformations", () => {
   describe("Temperature Analysis", () => {
-    it("should filter temperatures greater than or equal to 25 degrees Celsius", () => {
-      expect(filterHighTemperatures(temperatures)).toEqual([
-        25, 28, 30, 27, 29, 26, 28, 30, 27, 26, 25, 28, 29, 27, 26,
-      ]);
+    describe("filterHighTemperatures", () => {
+      it("should filter temperatures greater than or equal to 25 degrees Celsius", () => {
+        expect(filterHighTemperatures(temperatures)).toEqual([
+          25, 28, 30, 27, 29, 26, 28, 30, 27, 26, 25, 28, 29, 27, 26,
+        ]);
+      });
     });
 
-    it("should filter temperatures less than 20 degrees Celsius", () => {
-      expect(filterLowTemperatures(temperatures)).toEqual([19, 18, 19, 19]);
+    describe("filterLowTemperatures", () => {
+      it("should filter temperatures less than 20 degrees Celsius", () => {
+        expect(filterLowTemperatures(temperatures)).toEqual([19, 18, 19, 19]);
+      });
     });
   });
 
   describe("Temperature Transformation", () => {
-    it("should convert temperatures to Fahrenheit", () => {
-      expect(convertCelsiusToFahrenheit([22, 25])).toEqual([71.6, 77]);
+    describe("convertCelsiusToFahrenheit", () => {
+      it("should convert temperatures to Fahrenheit", () => {
+        expect(convertCelsiusToFahrenheit([22, 25])).toEqual([71.6, 77]);
+      });
     });
 
-    it("should label temperatures correctly", () => {
-      expect(labelTemperatures([22, 25, 19])).toEqual(["Mild", "Warm", "Cool"]);
+    describe("labelTemperatures", () => {
+      it("should label temperatures correctly", () => {
+        expect(labelTemperatures([22, 25, 19])).toEqual([
+          "Mild",
+          "Warm",
+          "Cool",
+        ]);
+      });
     });
   });
 
   describe("Summary Statistics", () => {
-    it("should find the highest temperature of the month", () => {
-      expect(getMaxTemperature(temperatures)).toBe(30);
+    describe("getMaxTemperature", () => {
+      it("should find the highest temperature of the month", () => {
+        expect(getMaxTemperature(temperatures)).toBe(30);
+      });
     });
 
-    it("should find the lowest temperature of the month", () => {
-      expect(getMinTemperature(temperatures)).toBe(18);
+    describe("getMinTemperature", () => {
+      it("should find the lowest temperature of the month", () => {
+        expect(getMinTemperature(temperatures)).toBe(18);
+      });
     });
   });
 });
